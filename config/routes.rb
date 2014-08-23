b6fcc470e2e76943c22a7ee970ebd6426b035cda
Craig Webster <craig.webster@mac.com>
2014-08-23 19:08:41 +0100

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'nmlc/index'
+  root 'nmlc#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
